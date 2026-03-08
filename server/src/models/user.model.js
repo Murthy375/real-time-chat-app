631da42cd8d179266d59076df04519065b749d19
Sridhar Murthy K Chandra <sridharkc375@gmail.com>
2026-03-08 22:59:29 +0530

    refactor(models): update user model to include unique name constraint
    - add unique constraint to the name field in the user model, as it will be used in jwt payload and it should be unique for each user

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 const userSchema = new Schema(
   {
-    name: { type: String, required: true, trim: true },
+    name: { type: String, required: true, trim: true, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String},
     email: { type: String, required: true, unique: true },
